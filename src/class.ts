dc7e55e4d69d6c5565ca02c4ee7801a7fb7f2432
sumonkhan0077 <sumonkhanbd0077@gmail.com>
2026-05-11 19:24:07 +0600

    added new problem of  typescript

--- src/class.ts
+++ src/class.ts
@@ -31,4 +31,39 @@ class Amimal {
 const dog = new Amimal ("dogeee vaiya", "dog" , "ghew gheew ")
 dog.makeSound()
 
+
+class Student {
+  id: number;
+  name: string;
+  marks: number[];
+
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+    this.marks = [];
+  }
+
+  addMarks(mark: number): void {
+    this.marks.push(mark);
+  }
+
+  getTotalMarks(): number {
+    return this.marks.reduce((total, mark) => total + mark, 0);
+  }
+
+  getAverage(): number {
+    const total = this.getTotalMarks();
+
+    return total / this.marks.length;
+  }
+}
+
+const student1 = new Student(1, "Sumon");
+
+student1.addMarks(80);
+student1.addMarks(90);
+student1.addMarks(70);
+
+console.log(student1.getTotalMarks());
+console.log(student1.getAverage());
  
